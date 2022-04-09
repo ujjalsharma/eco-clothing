c5b9413c523a3155b78c907fd90606115e845295
Ujjal Sharma <ujjalsharma81@gmail.com>
2022-04-09 17:05:13 +0530

    used react-router-dom to add navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Route, Switch } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+
+
+const HatsPage = () => (
+  
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+        <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop/hats" component={HatsPage} />
+        </Switch>
     </div>
   );
 }
