023c366d2281f55be83625053a28b85811a149ed
Ujjal Sharma <ujjalsharma81@gmail.com>
2022-04-16 15:44:39 +0530

    added redux and userReducer and userActions

--- src/App.js
+++ src/App.js
@@ -9,28 +9,24 @@ import Header from './components/header/header.component';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import React from 'react';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 class App extends React.Component {
-  
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapshot.id,
               ...snapshot.data()
@@ -40,7 +36,7 @@ class App extends React.Component {
 
       }
 
-      this.setState({currentUser: userAuth});
+      setCurrentUser(userAuth);
       
     });
   }
@@ -55,7 +51,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-          <Header currentUser={this.state.currentUser} />
+          <Header />
           <Switch>
               <Route exact path="/" component={HomePage} />
               <Route exact path="/shop" component={ShopPage} />
@@ -67,4 +63,8 @@ class App extends React.Component {
   
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { ReactComponent as Logo } from "../../assets/crown.svg"
 import { auth } from "../../firebase/firebase.utils";
 import './header.styles.scss'
+import { connect } from "react-redux";
 
 const Header = ({currentUser}) => (
 
@@ -29,4 +30,8 @@ const Header = ({currentUser}) => (
 
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
