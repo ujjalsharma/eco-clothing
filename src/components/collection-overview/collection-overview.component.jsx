f116b072ea2373beee4914611e8aa563b6d3a6c9
Ujjal Sharma <ujjalsharma81@gmail.com>
2022-04-19 16:37:09 +0530

    used redux for other componenets, added localstorage and created collection overview componenet

---
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect} from "react-redux";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { createStructuredSelector } from "reselect";
+import { selectShopCollection } from "../../redux/shop/shop.selector";
+import './collection-overview.styles.scss'
+
+const CollectionOverView = ({collections}) => (
+    <div className="collection-overview">
+         {
+            collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
+        } 
+    </div>
+
+);
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopCollection
+});
+
+export default connect(mapStateToProps)(CollectionOverView);
--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store} >
-  <BrowserRouter><App /></BrowserRouter>
+  <BrowserRouter><PersistGate persistor={persistor}><App /></PersistGate></BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
